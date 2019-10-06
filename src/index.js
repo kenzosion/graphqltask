1276e267174e44d1f69f25f72dbe660e01a20b29
Haur Kang <haur.kang@gmail.com>
2019-10-06 21:36:43 +0100

    import GraphQLServer

--- src/index.js
+++ src/index.js
@@ -1,5 +1,33 @@
 import { message } from './theDom';
 import subs, { add } from './math';
+import { GraphQLServer } from 'graphql-yoga';
+
+// type definitions (schema)
+
+const typeDefs = `
+    type Query {
+        hello: String!
+    }
+`
+
+// Resolver
+
+const resolvers = {
+    Query: {
+        hello () {
+            return 'This is a resolver!'
+        }
+    }
+}
+
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers
+})
+
+server.start(()=> {
+    console.log('this is from the server')
+})
 
 
 
