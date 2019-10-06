03b89e2a238ea102ef88518c3792739172021744
Haur Kang <haur.kang@gmail.com>
2019-10-06 21:21:22 +0100

    add math file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import { message } from './theDom';
+import subs, { add } from './math';
 
 
 
+console.log(add(5, 6));
+console.log(subs(10, 5));
 console.log(message);
---
+++ src/math.js
@@ -0,0 +1,4 @@
+const add = ( x, y) => x+y ;
+const subs = ( x, y ) => x-y;
+
+export { add, subs as default } 
